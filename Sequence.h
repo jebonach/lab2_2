b8de3e4f02c6974f2a1734457406de7d6186d00a
jebonach <bonnie.2.fnaf@gmail.com>
2025-03-30 20:29:58 +0300

    added sequence.h

---
+++ Sequence.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <stdexcept>
+
+template <class T>
+class Sequence {
+public:
+    virtual T GetFirst() const = 0;
+    virtual T GetLast() const = 0;
+    virtual T Get(int index) const = 0;
+    virtual int GetLength() const = 0;
+
+    virtual Sequence<T>* GetSubsequence(int startIndex, int endIndex) const = 0;
+    virtual Sequence<T>* Append(const T& item) = 0;
+    virtual Sequence<T>* Prepend(const T& item) = 0;
+    virtual Sequence<T>* InsertAt(const T& item, int index) = 0;
+    virtual Sequence<T>* Concat(const Sequence<T>* seq) const = 0;
+
+    virtual Sequence<T>* Clone() const = 0;
+    virtual ~Sequence() {}
+};
---
+++ ui.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "ui.h"
+// #include "ListSequence.h"
+
+// static void handlePushFront(ListSequence<int>& listSeq) {
+    
+// }
